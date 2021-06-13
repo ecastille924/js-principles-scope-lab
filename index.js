0a0fc32a27ffe7d1c411a2bfcca065b2d2b60a3e
Erick Castille <ecastille924@gmail.com>
2021-06-13 12:26:10 -0400

    commit tree 4ddd8e78a4f0ca0f6d9e6a8d93e296fea327c888

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-var customerName = "bob";
+// var customerName = "bob";
 
 
-function upperCaseCustomerName() {
-    return customerName = customerName.toUpperCase();
+// function upperCaseCustomerName() {
+//     return customerName = customerName.toUpperCase();
+// }
+
+// function setBestCustomer() {
+//     bestCustomer = "not bob";
+// }
+
+// function overwriteBestCustomer() {
+//     bestCustomer = "maybe bob";
+// }
+
+// const leastFavoriteCustomer = "Carol";
+
+// function changeLeastFavoriteCustomer() {
+//     leastFavoriteCustomer = "Doug";
+// }
+
+
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+   return customerName = customerName.toUpperCase()
 }
 
 function setBestCustomer() {
-    bestCustomer = "not bob";
+     bestCustomer = "not bob"
 }
 
 function overwriteBestCustomer() {
-    bestCustomer = "maybe bob";
+    bestCustomer = "maybe bob"
 }
 
-const leastFavoriteCustomer = "Carol";
+const leastFavoriteCustomer = "Randy"
 
 function changeLeastFavoriteCustomer() {
-    leastFavoriteCustomer = "Doug";
+     leastFavoriteCustomer = "Crandall"
 }
-
